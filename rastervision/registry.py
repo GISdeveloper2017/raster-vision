613e4014b6550f6118da70ee714ea543b8b13cdf
Lewis Fishgold <lewfish@gmail.com>
2018-10-26 14:35:23 -0400

    Fix evaluator typo

--- rastervision/registry.py
+++ rastervision/registry.py
@@ -132,7 +132,7 @@ class Registry:
         ]
 
         self._internal_default_evaluators = [
-            ObjectDetectioneEvaluatorDefaultProvider,
+            ObjectDetectionEvaluatorDefaultProvider,
             ChipClassificationEvaluatorDefaultProvider,
             SemanticSegmentationEvaluatorDefaultProvider
         ]
